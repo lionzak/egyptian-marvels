c2c62067983028b5e87309cbe3b3d244eb0776a4
Mark Alber <markalber59@gmail.com>
2025-07-12 21:14:37 +0300

    removed useEffect

--- components/GlobalAudioPlayer.tsx
+++ components/GlobalAudioPlayer.tsx
@@ -1,5 +1,5 @@
     'use client'
-    import { useEffect, useRef, useState } from 'react'
+    import {useRef, useState } from 'react'
 
     const GlobalAudioPlayer = () => {
     const audioRef = useRef<HTMLAudioElement>(null)
